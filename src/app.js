e0c11ea60f31ad7e04c1bd9e5b0dcb114c644a68
Luke Davis <lukefable@gmail.com>
2014-12-17 02:59:05 +0000

    changed the initial camera angle to something more charismatic.

--- src/app.js
+++ src/app.js
@@ -105,6 +105,8 @@ app.v.initScene=function(){
 
         camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 1, 10000 );
         camera.position.z = 1000;
+        camera.position.y = 600;
+        camera.position.x = 1000;
 
         controls = new THREE.OrbitControls( camera );
         controls.addEventListener( 'change', render );
